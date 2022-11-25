f38b19fc2c4258e34493000703601e028ae30f2c
jhlee <jihoonl@gmail.com>
2022-11-25 17:02:15 +0900

    [guest] coding 은 했으나, 작동은 아직 안함.

--- frontend/guest.js
+++ frontend/guest.js
@@ -1,10 +1,12 @@
 import {KakaoConfig, KakaoUrl} from './kakaocfg.js';
 
 let room = null;
+const roomId = KakaoConfig.roomId;
 let remoteParticipants = []; // guest 쪽에서 필요함.
-let localMedia = null;
 
-const logConsole = $('#logconsole');
+// const logConsole = $('#logconsole');
+const buttonConnect = document.querySelector('#connect-host');
+const buttonDisconnect = document.querySelector('#disconnect-host');
 
 const addLog = (text) => {
     const newLI = document.createElement('li');
@@ -13,6 +15,23 @@ const addLog = (text) => {
 
     let ol = document.getElementById('logconsole');
     ol.appendChild(newLI);
+}
+
+const resetLog = () => {
+    let ol = document.getElementById('logconsole');
+    while(ol.firstChild) {
+        ol.removeChild(ol.lastChild);
+    }
+}
+
+const activateButton = () => {
+    buttonConnect.disabled = false;
+    buttonDisconnect.disabled = true;
+}
+
+const disableButton = () => {
+    buttonConnect.disabled = true;
+    buttonDisconnect.disabled = false;
 }
 
 const addRemoteVideoNode = (videos) => {
@@ -35,97 +54,137 @@ const addRemoteVideoNode = (videos) => {
     });
 }
 
-// 카카오 라이브에 로그인
-const roomId = KakaoConfig.roomId;
-const ret = await ConnectLive.signIn({
-    serviceId: KakaoConfig.serviceId,
-    serviceSecret: KakaoConfig.serviceSecret
-});
-
-addLog('signIn . ret=' + ret);
-
-
-// 채팅방 접속/이벤트 처리하기
-room = await ConnectLive.createRoom();
-if (!room) {
-    // connectButton.innerHTML = 'failed to create room'
-    throw new Error('Failed to create room');
-}
-
-room.on('connected', async (evt) => {
-    // connectButton.innerHTML = 'connected'
-    if (!evt.remoteParticipants.length) {
-        addLog('No webinar starts yet');
-    }
-
-    evt.remoteParticipants.forEach(async (participant) => {
-        let videos = [];
-        const unsubscribedVideos = participant.getUnsubscribedVideos();
-
-        if (unsubscribedVideos.length) {
-            const videoIds = unsubscribedVideos.map((video) => video.getVideoId());
-            videos = await room.subscribe(videoIds);
-        }
-
-        remoteParticipants.push({participant, videos});
-        remoteParticipants.forEach((remoteParticipant) => {
-            const isSameId = remoteParticipant.participant.id === participant.id;
-            if (isSameId) {
-                addRemoteVideoNode(participant.videos);
-            }
-        });
-    });
-});
-
-room.on('remoteVideoPublished', async (evt) => {
-    const videos = await room.subscribe([evt.remoteVideo.videoId]);
-
-    if (video.length) {
-        const preJoinedParticipant = remoteParticipants.find(
-            (item) => item.participant.id === evt.remoteParticipant.id
-        );
-
-        if (preJoinedParticipant) {
-            preJoinedParticipant.videos = preJoinedParticipant.videos.concat(videos);
-        }
-    }
-
-    addRemoteVideoNode(videos);
-});
-
-room.on('remoteVideoUnpublished', async (evt) => {
-
-    const participantToRemove = remoteParticipants.find(
-        (item) => item.participant.id === evt.remoteParticipant.id
-    );
-
-    if (participantToRemove) {
-        participantToRemove.videos = participantToRemove.videos.filter(
-            (video) => video.videoId !== evt.remoteVideo.videoId
-        );
-
-        // disConnect.conference()
+const disConnect = {
+    conference() {
         room?.disconnect();
         addLog('Conference Disconnected');
-
-        // disConnect.remoteParticipants()
+    },
+    remoteParticipants() {
         remoteParticipants = [];
-        // removeRemoteVideoAll();
-        const remoteContainer = document.querySelector('.remote-container');
-        remoteContainer.innerHTML = '';
+        removeRemoteVideoAll();
         addLog('Participants Cleared');
-
-        // disConnect.user()
+    },
+    user() {
         ConnectLive.signOut();
-        addLog('User Signed OUt');
+        addLog('User Signed Out');
+    },
+    buttonStatus() {
+        activateButton();
+        changeStatus('Disconnected');
+    },
+};
 
-        addLog(`${evt.remoteParticipant.id} Left`);
+const createConferenceGuest = (room) => {
+    room.on('connected', async (evt) => {
+        // connectButton.innerHTML = 'connected'
+        if (!evt.remoteParticipants.length) {
+
+            disConnect.conference();
+            disConnect.user();
+            
+            addLog('No webinar starts yet');
+        }
+    
+        evt.remoteParticipants.forEach(async (participant) => {
+            let videos = [];
+            const unsubscribedVideos = participant.getUnsubscribedVideos();
+    
+            if (unsubscribedVideos.length) {
+                const videoIds = unsubscribedVideos.map((video) => video.getVideoId());
+                videos = await room.subscribe(videoIds);
+            }
+    
+            remoteParticipants.push({participant, videos});
+            remoteParticipants.forEach((remoteParticipant) => {
+                const isSameId = remoteParticipant.participant.id === participant.id;
+                if (isSameId) {
+                    addRemoteVideoNode(participant.videos);
+                }
+            });
+        });
+    });
+    
+    room.on('remoteVideoPublished', async (evt) => {
+        const videos = await room.subscribe([evt.remoteVideo.videoId]);
+    
+        if (video.length) {
+            const preJoinedParticipant = remoteParticipants.find(
+                (item) => item.participant.id === evt.remoteParticipant.id
+            );
+    
+            if (preJoinedParticipant) {
+                preJoinedParticipant.videos = 
+                    preJoinedParticipant.videos.concat(videos);
+            }
+        }
+    
+        addRemoteVideoNode(videos);
+    });
+    
+    room.on('remoteVideoUnpublished', async (evt) => {
+    
+        const participantToRemove = remoteParticipants.find(
+            (item) => item.participant.id === evt.remoteParticipant.id
+        );
+    
+        if (participantToRemove) {
+            participantToRemove.videos = participantToRemove.videos.filter(
+                (video) => video.videoId !== evt.remoteVideo.videoId
+            );
+    
+            disConnect.conference();
+            disConnect.remoteParticipants();
+            disConnect.user();
+            activateButton();
+    
+            addLog(`${evt.remoteParticipant.id} Left`);
+        }
+    });
+    
+};
+
+const connectConference = async (event) => {
+
+    try {
+
+        resetLog();
+        disableButton();
+
+        // Provisioning (카카오 라이브에 로그인)
+        const ret = await ConnectLive.signIn({
+            serviceId: KakaoConfig.serviceId,
+            serviceSecret: KakaoConfig.serviceSecret
+        });
+
+        addLog('signIn . ret=' + ret);
+
+        // 채팅방 접속/이벤트 처리하기
+        room = await ConnectLive.createRoom();
+        if (!room) {
+            // connectButton.innerHTML = 'failed to create room'
+            throw new Error('Failed to create room');
+        }
+
+        //
+        createConferenceGuest(room);
+
+        addLog('Conference Guest Created');
+
+        // Connect to room ( conference )
+        await room.connect(roomId);
     }
-});
+    catch(err) {
+        addLog('connectConference error=' + err);
 
 
-// Connect to room ( conference )
-await room.connect(roomId);
+    }
+}
+
+const disconnectConference = () => {
+
+};
+
+
 
 // add Local Video Node (render HTML)
 // const localContainer = $('#local-container');
@@ -145,3 +204,6 @@ await room.connect(roomId);
 // localContainer.appendChild(videoItem);
 
 // addLog(localContainer);
+
+buttonConnect.onclick = connectConference;
+buttonDisconnect.onclick = disconnectConference;
